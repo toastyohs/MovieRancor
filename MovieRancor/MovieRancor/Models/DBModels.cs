954ec0de0849337323063c898f6d93c7e873be68
Ricky <toastyohs@gmail.com>
2015-08-14 15:57:37 -0700

    code first database design

---
+++ MovieRancor/MovieRancor/Models/DBModels.cs
@@ -0,0 +1,132 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MovieRancor.Models
+{
+    public enum role { banned, user, moderator, admin, developer };
+    public enum workerRole { actor, director, writer };
+    public enum CategoryType { Genre, Series, Other }
+
+    //The u
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+
+
+        public virtual ICollection<Category> Categories { get; set; }
+        public virtual ICollection<MovieWorker> Workers { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<Score> Scores { get; set; }
+    }
+    //Allows the movie to pull the actors rather than hard coding them in there, also allows search by actors if that is to be added later
+    public class MovieWorker
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public workerRole Role { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        public virtual Movie Movie { get; set; }
+        public virtual ApplicationUser Reviewer { get; set; }
+        public virtual Score Score { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CommentDate { get; set; }
+
+        public virtual ApplicationUser Commenter { get; set; }
+        public virtual Review Review { get; set; }
+    }
+
+    public class Score
+    {
+        [Key]
+        public int Id { get; set; }
+        [Range(1, 5)]
+        public int score { get; set; }
+
+        public virtual ICollection<Movie> Movie { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+    }
+
+    public class Role
+    {
+        [Key]
+        public int UserId { get; set; }
+        public role UserRole { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+
+    public class List
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ApplicationUser Creator { get; set; }
+
+        public virtual ICollection<Movie> movies { get; set; }
+    }
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public CategoryType Type { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+            : base("MovieContext")
+        { }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<MovieWorker> MovieWorkers { get; set; }
+        public DbSet<List> Lists { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+    public class DatabaseContext : IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext()
+            : base("DatabaseContext", throwIfV1Schema: false)
+        {
+        }
+
+        public static DatabaseContext Create()
+        {
+            return new DatabaseContext();
+        }
+    }
+}
--- MovieRancor/MovieRancor/Models/IdentityModels.cs
+++ MovieRancor/MovieRancor/Models/IdentityModels.cs
@@ -3,12 +3,20 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace MovieRancor.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string IpAddress { get; set; }
+        public string EmailAddress { get; set; }
+        public string AvatarPath { get; set; } //not too sure on this one, just throwing it out there for Ss & Gs
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Score> Scores { get; set; }
+        public virtual ICollection<List> Lists { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -18,16 +26,4 @@ namespace MovieRancor.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
