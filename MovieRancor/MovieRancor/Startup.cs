a2801d36c815043e810d4d6ab0f1ca79afed56cd
Ricky <toastyohs@gmail.com>
2015-08-14 11:19:58 -0700

    Base project. Nothing really created yet

---
+++ MovieRancor/MovieRancor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRancor.Startup))]
+namespace MovieRancor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
